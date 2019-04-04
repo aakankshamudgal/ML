a1d34a312b7cc6bd3db2237b05a0b79bed667388
aakankshamudgal <aakankshamudgal09@gmail.com>
2019-04-04 20:47:53 +0530

    commit classself

---
+++ classself.py
@@ -0,0 +1,20 @@
+class Calculator:
+        def add(a,b):
+                return a+b
+        def sub(a,b):
+                if(a>b):
+                        return a-b
+                else:
+                        return b-a
+        def mul(a,b):
+                return a*b
+        def div(a,b):
+                return a/b
+        def mod(a,b):
+                return a%b
+        x=add(4,5)
+        print(x)
+        y=sub(6,7)
+        print(y)
+        z=mul(3,2)
+
