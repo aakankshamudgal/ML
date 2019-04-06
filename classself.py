7d0689eb1b23aaee6ab4922693c75bb687fd0bb3
aakankshamudgal <aakankshamudgal09@gmail.com>
2019-04-06 20:19:07 +0530

    edited

--- classself.py
+++ classself.py
@@ -1,20 +1,24 @@
 class Calculator:
-        def add(a,b):
+        def add(self,a,b):
                 return a+b
-        def sub(a,b):
+        def sub(self,a,b):
                 if(a>b):
                         return a-b
                 else:
                         return b-a
-        def mul(a,b):
+        def mul(self,a,b):
                 return a*b
-        def div(a,b):
+        def div(self,a,b):
                 return a/b
-        def mod(a,b):
+        def mod(self,a,b):
                 return a%b
-        x=add(4,5)
-        print(x)
-        y=sub(6,7)
-        print(y)
-        z=mul(3,2)
-
+x=Calculator().add(4,5)
+print(x)
+y=Calculator().sub(6,7)
+print(y)
+z=Calculator().mul(3,2)
+print(z)
+c=Calculator().div(4,2)
+print(c)
+d=Calculator().mod(5,3)
+print(d)
